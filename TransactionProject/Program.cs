56c8f0587d55fc9de96661f5fc98629fb45292e5
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-08-30 15:45:14 -0400

    Add project files.

---
+++ TransactionProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TransactionProject {
+    class Program {
+        static void Main(string[] args) {
+            
+           
+                Console.Write("Press Enter to Start Entering Transactions, type 'quit' to exit:  ");
+            string startup = Console.ReadLine();
+            while (startup != "quit") {
+
+                Console.Write("Enter First Name: ");
+                var response1 = Console.ReadLine();
+
+                Console.Write("Enter Last Name: ");
+                var response2 = Console.ReadLine();
+
+                Console.Write("Enter Sale Amount: ");
+                var response3 = Console.ReadLine();
+
+                var decresponse3 = decimal.Parse(response3);
+
+                Console.Write("Enter Card Number: ");
+                var response4 = Console.ReadLine();
+
+                var intresponse4 = double.Parse(response4);
+
+                var newTransaction = new Transaction(response1, response2, decresponse3, intresponse4);
+
+
+
+
+
+
+                var Transactions = new List<Transaction>();
+
+                Transactions.Add(newTransaction);
+            }
+              
+                
+            }
+        }
+            
+        }
+    
+
---
+++ TransactionProject/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransactionProject {
+    class Transaction {
+        private static int NextSaleId = 0;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal SaleAmount { get; set; } = 0;
+        public double CardNumber { get; set; }
+        public int SaleId { get; private set; }
+
+        public Transaction() {
+            SaleId = ++NextSaleId;     
+        }
+
+        public Transaction(string firstname, string lastname, decimal saleamount, double cardnumber) : this() {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.SaleAmount = saleamount;
+            this.CardNumber = cardnumber;
+
+
+        }
+
+        public override string ToString() {
+            return $"{SaleId, -15} {FirstName, -14} {LastName, -13} {SaleAmount,-12} {CardNumber, -11}";
+        }
+
+    }
+}
